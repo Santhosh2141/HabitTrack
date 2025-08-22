78884e2d5116cdb76c37838fd8fb1bd736b1307c
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-22 22:06:22 +0530

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Santhosh Srinivas on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
